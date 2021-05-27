880011fe044abf1b3f8598ccb4d9db30788ed5da
201602010 <bumsuh05@gmail.com>
2021-05-27 17:29:35 +0900

    Fix MongoDB

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, Response, request
+from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(host='localhost', port=27017)
+db = client['test']
+
 @app.route("/")
 def index():
+    print(db)
     return 'Hello, World!'
 
 if __name__ == "__main__":
